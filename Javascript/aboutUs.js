8a31aa8e06888b8003da5a413fdabccdfdf51149
Silfverlin <43538895+Silfverlin@users.noreply.github.com>
2018-09-30 19:45:45 +0200

    Add files via upload

--- Javascript/aboutUs.js
+++ Javascript/aboutUs.js
@@ -1,7 +1,5 @@
 $(document).ready(() => {
-	
 
-	
 	function listJava() {
 		fetch('https://github-trending-api.now.sh/repositories?language=javascript&since=weekly') //fetch returns a promise
 		.then((response) => response.json())
@@ -11,18 +9,16 @@ $(document).ready(() => {
 			while(index < 5) {
 				var tableRow = $(		
 					`<tr>
-						<td><a href="${data[index].url}">${data[index].author}</a></td>					
-						<td><a href="${data[index].url}">${data[index].name}</a></td>
-						<td>${data[index].stars}</td>	
+						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].author}</a></td>					
+						<td class="list__tableContent"><a class="list__tableLink"href="${data[index].url}">${data[index].name}</a></td>
+						<td class="list__tableContent">${data[index].stars}</td>	
 					</tr>`);
-				$('#listJava-table').append(tableRow);
+				$('#listJava').append(tableRow);
 				index++;	
 			}
 		})
 	}
 	listJava();
-
-
 
 	function listCss() {
 		fetch('https://github-trending-api.now.sh/repositories?language=css&since=weekly ') //fetch returns a promise
@@ -33,11 +29,11 @@ $(document).ready(() => {
 			while(index < 5) {
 				var tableRow = $(		
 					`<tr>
-						<td>${data[index].author}</td>					
-						<td>${data[index].name}</td>
-						<td>${data[index].stars}</td>	
+						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].author}</td>					
+						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].name}</td>
+						<td class="list__tableContent">${data[index].stars}</td>	
 					</tr>`);
-				$('#listCss-table').append(tableRow);
+				$('#listCss').append(tableRow);
 				index++;	
 			}
 		})
