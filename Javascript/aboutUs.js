f1d7b5aac9f87cd463615d9f0844713486f485f0
Silfverlin <43538895+Silfverlin@users.noreply.github.com>
2018-10-01 11:18:57 +0200

    Add files via upload

--- Javascript/aboutUs.js
+++ Javascript/aboutUs.js
@@ -3,14 +3,14 @@ $(document).ready(() => {
 	function listJava() {
 		fetch('https://github-trending-api.now.sh/repositories?language=javascript&since=weekly') //fetch returns a promise
 		.then((response) => response.json())
-		.then((data) => {
+		.then((data) => { 																			//do something with data
 		//looping through index 0-4 of the array
 			var index = 0;
 			while(index < 5) {
 				var tableRow = $(		
 					`<tr>
 						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].author}</a></td>					
-						<td class="list__tableContent"><a class="list__tableLink"href="${data[index].url}">${data[index].name}</a></td>
+						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].name}</a></td>
 						<td class="list__tableContent">${data[index].stars}</td>	
 					</tr>`);
 				$('#listJava').append(tableRow);
