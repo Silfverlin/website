fb5b04fac7de64b03ef79ef74d16bb11d83d8d11
PieceOfSteel <43567912+PieceOfSteel@users.noreply.github.com>
2018-10-02 10:38:49 +0200

    Add files via upload

--- Javascript/aboutUs.js
+++ Javascript/aboutUs.js
@@ -7,9 +7,10 @@ $(document).ready(() => {
 		//looping through index 0-4 of the array
 			var index = 0;
 			while(index < 5) {
+				var authorLink = getAuthorLink(data[index].author);
 				var tableRow = $(		
 					`<tr>
-						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].author}</a></td>					
+						<td class="list__tableContent"><a class="list__tableLink" href="${authorLink}">${data[index].author}</a></td>					
 						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].name}</a></td>
 						<td class="list__tableContent">${data[index].stars}</td>	
 					</tr>`);
@@ -27,9 +28,10 @@ $(document).ready(() => {
 		//looping through index 0-4 of the array
 				var index = 0;
 			while(index < 5) {
-				var tableRow = $(		
+				var authorLink = getAuthorLink(data[index].author);
+				var tableRow = $(
 					`<tr>
-						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].author}</td>					
+						<td class="list__tableContent"><a class="list__tableLink" href="${authorLink}">${data[index].author}</td>					
 						<td class="list__tableContent"><a class="list__tableLink" href="${data[index].url}">${data[index].name}</td>
 						<td class="list__tableContent">${data[index].stars}</td>	
 					</tr>`);
@@ -40,4 +42,9 @@ $(document).ready(() => {
 	}
 	listCss();
 
+	function getAuthorLink() {
+		var author = arguments[0];
+		var authorLink = `http://www.github.com/${author}/`;
+		return authorLink;
+	}
 })
